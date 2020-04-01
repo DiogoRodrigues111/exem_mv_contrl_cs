d4b8226a7029a45e181f088785ff02af190f8b27
Diogo Roessler <drroessler@hotmail.com.br>
2020-04-01 15:34:44 -0300

    Add files via upload

---
+++ Imagem/Form1.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Input;
+
+namespace Imagem
+{
+    public partial class Form1 : Form
+    {
+        Point local;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void MouseDownEvent(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button==MouseButtons.Left)
+            {
+                local=e.Location;
+            }
+        }
+
+        private void MouseMoveEvent(object sender, System.Windows.Forms.MouseEventArgs e)
+        {
+            if (e.Button==MouseButtons.Left)
+            {
+                panel1.Left=e.X+panel1.Left-local.X;
+                panel1.Top=e.Y+panel1.Top-local.Y;
+            }
+        }
+    }
+}
---
+++ Imagem/IMove.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Imagem
+{
+    public interface IMove
+    {
+        uint Movement(int x, int y);
+        bool MouseTrack(int x, int y);
+    }
+}
